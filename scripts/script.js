8e746f21098c8a8c963d7a7b09135d9a34e7bf93
JuIcEPoopooumgood <josifbell@gmail.com>
2024-11-07 15:37:52 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const userID = "738748102311280681"; // Thay đổi thành ID Discord của bạn
+const userID = "1169111190824308768"; // Thay đổi thành ID Discord của bạn
 
 const elements = {
 	statusBox: document.querySelector(".status"),
